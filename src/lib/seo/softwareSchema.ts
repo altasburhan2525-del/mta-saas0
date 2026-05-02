e7a59f7e629df2a96c435e87bae3dcd122a530ba
altasburhan2525-del <altasburhan2525@gmail.com>
2026-05-03 00:04:02 +0300

    Add SoftwareApplication schema data

---
+++ src/lib/seo/softwareSchema.ts
@@ -0,0 +1,17 @@
+export const softwareApplicationSchema = {
+  '@context': 'https://schema.org',
+  '@type': 'SoftwareApplication',
+  name: 'MT Altaş Teklif ve Maliyet Hesaplayıcı',
+  applicationCategory: 'BusinessApplication',
+  operatingSystem: 'Web',
+  description: 'Kilit taşı, metraj, nakliye, yakıt ve teklif maliyeti hesaplama odaklı profesyonel web yazılımı.',
+  offers: {
+    '@type': 'Offer',
+    price: '0',
+    priceCurrency: 'TRY',
+  },
+  publisher: {
+    '@type': 'Organization',
+    name: 'MT Altaş',
+  },
+};
